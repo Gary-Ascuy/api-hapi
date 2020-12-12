7f232671773af490f0a49830e7d6781164acf381
Gary Ascuy <gary.ascuy@gmail.com>
2020-12-12 16:58:10 -0500

    Adding docs for settings

--- src/settings/index.js
+++ src/settings/index.js
@@ -1,5 +1,15 @@
 const nconf = require('nconf')
 
+/**
+ * ENVIRONMENT VARIABLES
+ *   export server__host=localhost
+ *   export server__port=5000
+ * 
+ * COMMAND LINE INTERFACE
+ *   node src/index.js --server:port 5000
+ *   yarn start --server:host 127.0.0.1
+ */
+
 nconf.argv()
    .env('__')
    .file('./src/settings/defaults.json')
