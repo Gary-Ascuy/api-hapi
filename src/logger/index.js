583d040287583dec22bcacaadb47becc030e2811
Gary Ascuy <gary.ascuy@gmail.com>
2020-12-12 22:56:17 -0500

    Adding support for level in settings

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,17 +1,16 @@
+const { required } = require('nconf')
 const winston = require('winston')
 
-const logger = winston.createLogger(
-  {
-    transports:
-    [
-      new winston.transports.Console({
-        format: winston.format.combine(
-          winston.format.colorize(),
-          winston.format.simple(),
-        ),
-      }),
-    ],
-  },
+const { get } = require('../settings')
+
+const format = winston.format.combine(
+  winston.format.colorize(),
+  winston.format.simple(),
 )
+
+const logger = winston.createLogger({
+  level: get('logger.level'),
+  transports: [new winston.transports.Console({ format })],
+})
 
 module.exports = { logger }
