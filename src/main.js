93462695742061371305e476b9458fde9deecc75
Gary Ascuy <gary.ascuy@gmail.com>
2020-12-12 23:59:28 -0500

    Adding support for database, mongodb with mongoose

--- src/main.js
+++ src/main.js
@@ -2,18 +2,30 @@ const Hapi = require('@hapi/hapi')
 
 const { get } = require('./settings')
 const { logger } = require('./logger')
+const { connect, disconnect } = require('./app/models/database')
 
 async function main() {
+  await connect()
+
   const { host, port } = get('server')
   const server = Hapi.server({ port, host })
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => 'Hello World!'
+  })
 
   await server.start()
   logger.info(`Server running on ${server.info.uri}`)
 }
 
-process.on('unhandledRejection', (err) => {
-  logger.error(err)
+function exit(error) {
+  disconnect()
+  logger.error(error)
   process.exit(1)
-})
+}
+
+process.on('unhandledRejection', exit)
 
 module.exports = { main }
